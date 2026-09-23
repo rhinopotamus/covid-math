646961e75cf199e3cb6acc1e687da3447e3e9695
Spencer Bagley <orangefish@gmail.com>
2026-09-23 15:42:12 -0600

    Latest build deployed.

--- course/lunr-pretext-search-index.js
+++ course/lunr-pretext-search-index.js
@@ -7,7 +7,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "",
   "title": "Schedule of topics",
-  "body": " Schedule of topics  This schedule is tentative. We might move things around according to people's needs.    Date  Topics    Sep 3   Terms and factors!  The equals sign means two things are the same!     Sep 10   The many meanings of letters!  Variables, constants, parameters, and other tricks!     Sep 17    Lines and linear equations!  Slope-intercept and point-slope form!     Sep 24   Rules of exponents!  Adding, multiplying, and fractions!     Oct 1   Logarithms!  (The worst name in mathematics!)     Oct 8   Fractions and percents!  Numerators, denominators, and canceling !     Oct 15   Substitution:  Replacing something with another thing that's the same!     Oct 22  Fall break!    Oct 29     Nov 5     Nov 12     Nov 19     Nov 26  Thanksgiving!    Dec 3     Dec 10     Dec 17     "
+  "body": " Schedule of topics  This schedule is tentative. We might move things around according to people's needs.    Date  Topics     Sep 17    Lines and linear equations!  Slope-intercept and point-slope form!     Sep 24   Terms and factors!  The equals sign means two things are the same!     Oct 1   Rules of exponents!  Adding, multiplying, and fractions!     Oct 8   Logarithms!  (The worst name in mathematics!)     Oct 15   Fractions and percents!  Numerators, denominators, and canceling !     Oct 22  Fall break!    Oct 29   Substitution:  Replacing something with another thing that's the same!     Nov 5     Nov 12     Nov 19     Nov 26  Thanksgiving!    Dec 3     Dec 10     Dec 17     "
 }
 ]
 
